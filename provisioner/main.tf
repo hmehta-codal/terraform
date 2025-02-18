77670739ece77fa9f29e1b34a3d51d7e4af12426
hmehta-codal <hmehta@codal.com>
2025-02-18 14:30:06 +0530

    Configure ec2 using terraform

---
+++ provisioner/main.tf
@@ -0,0 +1,54 @@
+resource "aws_security_group" "example_sg" {
+  name        = "example-security-group"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a887e401f7654935"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+  key_name = "terraform"  
+
+
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/terraform.pem") 
+    host        = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install -y httpd",
+      "sudo systemctl start httpd"
+    ]
+  }
+  depends_on = [aws_security_group.example_sg]
+}
+
